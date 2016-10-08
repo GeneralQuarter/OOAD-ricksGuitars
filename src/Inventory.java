9c35b32b663faf4ec025f48d28feb81d4e2b67e1
Quentin Gangler <x1q.g1x@gmail.com>
2016-10-08 17:47:40 +0100

    Added the loading of the inventory via a file - Filled guitarsInInventory.txt - Modified the code to load automatically the guitars (in the constructor of Inventory)

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,12 +1,15 @@
+import java.io.File;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Inventory {
   private List guitars;
 
   public Inventory() {
     guitars = new LinkedList();
+    loadGuitarsFromFile();
   }
 
   public void addGuitar(String serialNumber, double price,
@@ -16,6 +19,7 @@ public class Inventory {
                                model, type, backWood, topWood);
     guitars.add(guitar);
   }
+
   public Guitar getGuitar(String serialNumber) {
     for (Iterator i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
@@ -25,6 +29,28 @@ public class Inventory {
     }
     return null;
   }
+
+  private void loadGuitarsFromFile() {
+    try {
+      File guitarsInInventoryFile = new File("guitarsInInventory.txt");
+      Scanner in = new Scanner(guitarsInInventoryFile);
+
+      while(in.hasNext()) {
+        String line = in.nextLine();
+        String[] fields = line.split(", ");
+        addGuitar(fields[0].replace("\"", ""),
+                Double.parseDouble(fields[1]),
+                fields[2].replace("\"", ""),
+                fields[3].replace("\"", ""),
+                fields[4].replace("\"", ""),
+                fields[5].replace("\"", ""),
+                fields[6].replace("\"", ""));
+      }
+    } catch (Exception e){
+      e.printStackTrace();
+    }
+  }
+
   public Guitar search(Guitar searchGuitar) {
     for (Iterator i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
